872dd22867175586752c7dbf298f9c63d29b212a
vagrant <vagrant@rails-dev-box>
2014-11-11 15:28:10 +0000

    First commit, README update and gem install

---
+++ README.md
@@ -0,0 +1,7 @@
+## Collabopedia
+Collabopedia is an application that allows users to create and collaborate on public and private Markdown-based wikis.
+
+Made with my mentor, Jon Linowes, at [Bloc](http://bloc.io).
+
+Collabopedia can be found hoted at the following Heroku address: <placeholder>
+<tt>rake doc:app</tt>.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Collabopedia::Application.config.secret_key_base = '32d95abbc113817f0649567b8a0405e957a6ddbee2f104f43a306540ce6924828b8612b2569bcf53df88c57605b70b747b4867048632341c28161f51e5f0177f'
