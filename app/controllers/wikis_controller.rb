cbfb1ddfb943017caeef9869447d0bd0235d5cc5
vagrant <vagrant@rails-dev-box>
2014-11-19 21:53:01 +0000

    pushing work for  review

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -13,13 +13,11 @@ class WikisController < ApplicationController
     @wiki = Wiki.new
     authorize @wiki 
   end
-   def create
+  def create
      #@wiki = Wiki.new(params.require(:wiki).permit(:title, :body))
       @wiki = current_user.wikis.build(params.require(:wiki).permit(:title, :body))
       @wiki.user_id = current_user.id
-
-
-     authorize @wiki 
+      authorize @wiki 
      if @wiki.save!
        flash[:notice] = "Wiki was saved."
        redirect_to @wiki
@@ -31,10 +29,12 @@ class WikisController < ApplicationController
 
   def edit
      @wiki = Wiki.find(params[:id])
+     authorize @wiki
   end
 
   def update
      @wiki = Wiki.find(params[:id])
+     authorize @wiki
      if @wiki.update_attributes(params.require(:wiki).permit(:title, :body))
        flash[:notice] = "Wiki was updated."
        redirect_to @wiki
