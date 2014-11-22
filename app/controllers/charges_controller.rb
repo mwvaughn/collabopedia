fb58698092abdde66d7b7deeb01f709ddd6e82c0
AlliVaughn <allisonlvaughn@gmail.com>
2014-11-22 13:21:48 -0600

    Added private checkbox to wikis view, added premium def to charges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -29,7 +29,7 @@ class ChargesController < ApplicationController
    
  
    flash[:success] = "Thanks for all the money, #{current_user.email}! Feel free to pay me again."
-   current_user.update_attributes(paid: true)
+   current_user.update_attributes(paid: Time.now)
    redirect_to user_session_path(current_user.name) # or wherever
  
  # Stripe will send back CardErrors, with friendly messages
