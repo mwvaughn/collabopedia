9074ca55f9be9968bea4b198e87f0a8310d9d806
AlliVaughn <allisonlvaughn@gmail.com>
2014-11-22 10:21:47 -0600

    fixed db version

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20141121182458) do
-=======
+
 ActiveRecord::Schema.define(version: 20141118211800) do
->>>>>>> fcac8dad8ca92a053c5ae62a976ae977f0c01bea
 
   create_table "collaborators", force: true do |t|
     t.integer  "user_id"
