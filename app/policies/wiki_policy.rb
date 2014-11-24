c62ca8bb0a72904364287241d475cf77789f66cf
AlliVaughn <allisonlvaughn@gmail.com>
2014-11-24 14:19:00 -0600

    chnaged pundit wiki policy file

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -1,16 +1,31 @@
 class WikiPolicy < ApplicationPolicy
+  def owner
+    post.user_id == user.id
+  end
   def index? 
     true
   end 
   def show?  
    true
   end 
+  def new?
+    true
+  end 
+  def create?  
+    new?
+  end  
   def destroy?
     user.present? && (record.user || user.admin)
   end 
   def edit? 
     true
-  end  
+  end
+  def update?
+    edit? 
+  end 
+   def edit?
+    owner
+  end     
   def private?
     user.premium? || user.admin?
   end 
